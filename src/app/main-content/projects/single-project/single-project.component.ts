2625601d1dc4b7362bc63ebb93dd4e8725449969
Tobias Reize <reizeto@web.de>
2024-09-04 12:12:32 +0200

    styled references-section and added some bug fixes

--- src/app/main-content/projects/single-project/single-project.component.ts
+++ src/app/main-content/projects/single-project/single-project.component.ts
@@ -1,14 +1,20 @@
 import { Component, Input } from '@angular/core';
 import { Project } from '../../../shared/interface/project.interface';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-single-project',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './single-project.component.html',
   styleUrl: './single-project.component.scss'
 })
 export class SingleProjectComponent {
 
   @Input() singleProject!: Project;
+  @Input() index!: number;
+
+  isOdd(number: number):boolean {
+    return (number % 2 != 0);
+  }
 }
--- src/app/main-content/references/references.component.ts
+++ src/app/main-content/references/references.component.ts
@@ -1,13 +1,50 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { SingleReferenceComponent } from './single-reference/single-reference.component';
 
 @Component({
   selector: 'app-references',
   standalone: true,
-  imports: [SingleReferenceComponent],
+  imports: [CommonModule],
   templateUrl: './references.component.html',
   styleUrl: './references.component.scss'
 })
 export class ReferencesComponent {
 
+  currentReview = 0;
+
+  testimonials = [
+    {
+      person: 'V. Schuster - Team Partner',
+      comment: 'Michael really kept the team together with his great organization and clear communication. We wouldn\'t have got this far without his commitment.',
+      image: 'person1.png'
+    },
+    {
+      person: 'E.Eichinger - Team Partner',
+      comment: 'Michi was a top team colleague at DA. His positive commitment and willingness to take on responsibility made a significant contribution to us achieving our goals.',
+      image: 'person2.png'
+    },
+    {
+      person: 'I.Nuber - Frontend Engineer',
+      comment: 'It was a great pleasure to work with Michael. He knows how to push and encourage team members to present the best work possible, always adding something to brainstorm. Regarding the well-being of group members, he was always present and available to listen and help others, with a great sense of humor as well.',
+      image: 'person3.png'
+    }
+  ];
+
+
+  nextTestimonial() {
+    if ((this.currentReview + 1) >= this.testimonials.length) {
+      this.currentReview = 0;
+    } else {
+      this.currentReview++;
+    }
+  }
+
+
+  previousTestimonial() {
+    if ((this.currentReview - 1) < 0) {
+      this.currentReview = this.testimonials.length - 1;
+    } else {
+      this.currentReview--;
+    }
+  }
 }

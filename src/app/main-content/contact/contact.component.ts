7ea1a11f7d74eb2f42669196fca0a8300d2e0dd0
Tobias Reize <reizeto@web.de>
2024-09-05 15:49:20 +0200

    updated contact-section

--- src/app/main-content/contact/contact.component.ts
+++ src/app/main-content/contact/contact.component.ts
@@ -1,12 +1,92 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnChanges, SimpleChanges } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
-export class ContactComponent {
+export class ContactComponent implements OnChanges {
 
+  nameInputStatus: 'ok' | 'notok' | 'empty' = 'empty';
+  emailInputStatus: 'ok' | 'notok' | 'empty' = 'empty';
+  messsageInputStatus: 'ok' | 'notok' | 'empty' = 'empty';
+
+  nameInput: string = '';
+  emailInput: string = '';
+  messageInput: string = '';
+
+  agreedPrivacyPolicy = false;
+  btnDisabled = true;
+
+
+  checkInput(id: string, inputData: string) {
+    switch (id) {
+      case 'name':
+        if (inputData.length <= 3) {
+          this.nameInputStatus = 'notok';
+        } else {
+          this.nameInputStatus = 'ok';
+        }
+        break;
+      
+      case 'email':
+        if (inputData.length <= 3) {
+          this.emailInputStatus = 'notok';
+        } else {
+          this.emailInputStatus = 'ok';
+        }
+        break;
+    
+      case 'message':
+        if (inputData.length <= 3) {
+          this.messsageInputStatus = 'notok';
+        } else {
+          this.messsageInputStatus = 'ok';
+        }
+        break;
+    }
+  }
+
+
+  setBorderStyle(status: string) {
+    switch (status) {
+      case 'ok':
+        return {'border-color': '#70E61C'};
+
+      case 'notok':
+        return {'border-color': '#E61C40'};
+
+      default:
+        return {};
+    }
+  }
+
+
+  toggleCheckbox() {
+    this.agreedPrivacyPolicy = !this.agreedPrivacyPolicy;    
+  }
+
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (this.nameInputStatus === 'ok' && this.emailInputStatus === 'ok' && this.messsageInputStatus === 'ok' && this.agreedPrivacyPolicy) {
+      this.btnDisabled = false;
+      console.log('btnDisabled = ', this.btnDisabled);
+    } else {
+      this.btnDisabled = true;
+      console.log('btnDisabled = ', this.btnDisabled);
+    }
+  }
+
+
+//   checkButton() {
+//     if (this.nameInputStatus === 'ok' && this.emailInputStatus === 'ok' && this.messsageInputStatus === 'ok' && this.agreedPrivacyPolicy) {
+//       this.btnDisabled = false;
+//     } else {
+//       this.btnDisabled = true;
+//     }
+//   }
 }

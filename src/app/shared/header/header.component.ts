c4de24efba1b44e7987613d3c948b5e7cb79fa39
Tobias Reize <reizeto@web.de>
2024-09-11 17:22:42 +0200

    finished responsive design mobile for hero, about-me and skills-section

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -12,16 +12,32 @@ import { TranslateModule, TranslateService } from '@ngx-translate/core';
 })
 export class HeaderComponent {
 
+  burgerMenu = false;
+
   constructor(public scrollerService: ScrollerService, private translate: TranslateService) { }
 
 
   scrollToId(id: string) {
     this.scrollerService.scrollToAnchor(id);
     this.scrollerService.scrollId = id;
+    this.burgerMenu = false;
+    document.body.style.overflow = 'unset'
   }
 
 
   useLanguage(lang: string) {
     this.translate.use(lang);
+    this.burgerMenu = false;
+    document.body.style.overflow = 'unset'
+  }
+
+
+  showBurgerMenu() {
+    this.burgerMenu = !this.burgerMenu;
+    if (this.burgerMenu) {
+      document.body.style.overflow = 'hidden'
+    } else {
+      document.body.style.overflow = 'unset'
+    }
   }
 }

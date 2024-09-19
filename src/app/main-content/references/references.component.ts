019b2a2e5d5d3b5e65c9fe1636f49536a0c6c113
Tobias Reize <reizeto@web.de>
2024-09-19 07:57:38 +0200

    added correct linkedin-link and removed references

--- src/app/main-content/references/references.component.ts
+++ src/app/main-content/references/references.component.ts
@@ -14,19 +14,19 @@ export class ReferencesComponent {
 
   testimonials = [
     {
-      person: 'V. Schuster - Team Partner',
-      comment: 'Michael really kept the team together with his great organization and clear communication. We wouldn\'t have got this far without his commitment.',
-      image: 'person1.png'
+      person: 'Person 1 - Team Partner',
+      comment: '<reference 1>',
+      image: 'person.png'
     },
     {
-      person: 'E.Eichinger - Team Partner',
-      comment: 'Michi was a top team colleague at DA. His positive commitment and willingness to take on responsibility made a significant contribution to us achieving our goals.',
-      image: 'person2.png'
+      person: 'Person 2 - Team Partner',
+      comment: '<reference 2>',
+      image: 'person.png'
     },
     {
-      person: 'I.Nuber - Frontend Engineer',
-      comment: 'It was a great pleasure to work with Michael. He knows how to push and encourage team members to present the best work possible, always adding something to brainstorm. Regarding the well-being of group members, he was always present and available to listen and help others, with a great sense of humor as well.',
-      image: 'person3.png'
+      person: 'Person 3 - Team Partner',
+      comment: '<reference 3>',
+      image: 'person.png'
     }
   ];
 

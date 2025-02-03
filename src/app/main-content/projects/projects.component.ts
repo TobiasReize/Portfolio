a3b60e16c1f8cb6801e734a3b6f9a6399740c88f
Tobias Reize <reizeto@web.de>
2025-02-03 13:36:10 +0100

    commented out coderr project

--- src/app/main-content/projects/projects.component.ts
+++ src/app/main-content/projects/projects.component.ts
@@ -38,14 +38,14 @@ export class ProjectsComponent {
       linkTest: 'https://dabubble.tobias-reize.de/',
       linkGithub: 'https://github.com/TobiasReize/DABubble.git'
     },
-    {
-      title: 'Coderr',
-      techstack: 'Django | Django REST | Python',
-      description: 'project_description_coderr',
-      image: 'coderr.png',
-      linkTest: 'https://coderr.tobias-reize.de/',
-      linkGithub: 'https://github.com/TobiasReize/coderr_backend.git'
-    }
+    // {
+    //   title: 'Coderr',
+    //   techstack: 'Django | Django REST | Python',
+    //   description: 'project_description_coderr',
+    //   image: 'coderr.png',
+    //   linkTest: 'https://coderr.tobias-reize.de/',
+    //   linkGithub: 'https://github.com/TobiasReize/coderr_backend.git'
+    // }
   ];
 
   
